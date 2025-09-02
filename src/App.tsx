c3036f04ff7c09b2d50aa9364b87b469b69ba6ae
Bishu Thapa <thapabishu435@gmail.com>
2025-09-03 00:02:50 +0545

    create a app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold  w-full text-center h-screen flex items-center justify-center">Back </h1>
+    
+    </>
+  )
+}
+
+export default App
