1c14fe2f18f420f54fdf557798bac01b706d5059
Bishu Thapa <thapabishu435@gmail.com>
2025-09-16 00:36:30 +0545

    update the navlinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import HomePage from "./pages/HomePage";
+import './index.css'
+//import CustomeButton from './components/CustomeButton'
+import SignUp from './pages/SignUp.tsx';
+import Login from './pages/Login.tsx';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard.tsx';
+import { Toaster } from "react-hot-toast";
+import Home from './pages/Dashboard/Home.tsx';
 
-const App = () => {
+
+function App() {
+
+  
+  
+  
+
   return (
     <>
-      <HomePage  />    
+    <Routes>
+        <Route path="/" element={< Login/>} />
+        <Route path='/login' element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Home" element={<Home />} />
+
+    </Routes>
+    <Toaster position="top-center" reverseOrder={false} />
     </>
-  )
+    
+  );
 }
 
+export default App
 
-export default App; 
