eca98c873bf498cd54c633c147aadac7bd744495
Bishu Thapa <thapabishu435@gmail.com>
2025-09-26 09:42:56 +0545

    update the app.txs

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import Dashboard from './pages/Dashboard/Dashboard.tsx';
 import { Toaster } from "react-hot-toast";
 import Home from './pages/Dashboard/Home.tsx';
 import Watch  from './pages/watch.tsx';
-
+import Upload from './pages/Dashboard/Upload.tsx';
+import NotValidUser from './pages/NotValidUser.tsx';
+import ProtectedLayout from './pages/protectedLayout.tsx';
+import NotFound from './pages/NotFound.tsx';
 
 function App() {
 
@@ -18,12 +21,21 @@ function App() {
   return (
     <>
     <Routes>
+      {/*Public routes*/}
         <Route path="/" element={< Login/>} />
         <Route path='/login' element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path='/not-valid' element={<NotValidUser />} />
+
+      {/*Protected routes*/}
+      <Route element={<ProtectedLayout />}>
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Home" element={<Home />} />
         <Route path='/watch/:_id' element={<Watch />} />
+        <Route path='/upload' element={<Upload />} />
+      </Route>
+
+       <Route path="*" element={<NotFound />} />
 
     </Routes>
     <Toaster position="top-center" reverseOrder={false} />
