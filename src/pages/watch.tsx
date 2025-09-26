b74250e2330f2d6e5f10feac442e6c4db5943c6c
Bishu Thapa <thapabishu435@gmail.com>
2025-09-26 09:46:45 +0545

    initial commit

--- src/pages/watch.tsx
+++ src/pages/watch.tsx
@@ -7,15 +7,15 @@ const Watch = () => {
   if (!video) return <p className="text-white">No video found</p>;
 
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col items-center">
-      <div className="w-full flex flex-col ml-48 mt-8">
+    <div className="bg-gray-900 h-screen display flex">
+      <div className="w-full flex flex-col content-center">
       <video
         controls
         autoPlay
         className="w-3/4 max-h-[80vh] rounded-lg"
         src={video.video}
       />
-      <h2 className="text-white mt-4 text-xl">{video.title}</h2>
+      <h2 className="text-white text-xl">{video.title}</h2>
       <p className="text-gray-400">{video.description}</p>
     </div>
     <div>
